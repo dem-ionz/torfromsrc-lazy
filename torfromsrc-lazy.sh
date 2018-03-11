174bb796d2595ff08a1ba18fc34964c0649ea2d1
wo0sh ~# <31750444+dem-ionz@users.noreply.github.com>
2018-03-10 23:49:34 -0500

    updated tor version
    Tor 0.3.2.10

--- torfromsrc-lazy.sh
+++ torfromsrc-lazy.sh
@@ -4,9 +4,10 @@ if ! [ $(id -u) = 0 ]; then
    echo "How the fuck am I supposed to do this shit without root accesss? Run me as root!"
    exit 1
 fi
+echo by wo0sh!
 read -n 1 -s -r -p "This script should install Tor with minimal effort, after it runs at the end of install, please kill it with CTRL-C and run it as a *NON-ROOT* user. Press ENTER to start."
-wget https://www.torproject.org/dist/tor-0.3.2.9.tar.gz
-tar xfvz tor-0.3.2.9.tar.gz && cd tor-0.3.2.9/
+wget https://www.torproject.org/dist/tor-0.3.2.10.tar.gz
+tar xfvz tor-0.3.2.10.tar.gz && cd tor-0.3.2.10/
 apt-get install build-essential libevent-dev libssl-dev -y
 ./configure && make && src/or/tor && make install
 echo tor should be installed now!
